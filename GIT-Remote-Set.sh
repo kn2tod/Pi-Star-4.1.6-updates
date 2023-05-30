1ef9e84876daf0d00b9cc64dbc651389f74c184e
Mark Prichard <93064569+kn2tod@users.noreply.github.com>
2023-05-29 23:35:41 -0400

    Update GIT-Remote-Set.sh
    Corrected repo reset process

--- GIT-Remote-Set.sh
+++ GIT-Remote-Set.sh
@@ -11,12 +11,12 @@ pgit=${pgit/andy/AndyTaylorTweet}
 pgit=${pgit/mark/kn2tod}
 echo ${pgit}
 
-# set to make repos updated?
-#if ! grep -q 'safe' /root/.gitconfig > /dev/null 2<&1 ; then
-#    git config --global --add safe.directory /usr/local/sbin > /dev/null 2<&1
-#    git config --global --add safe.directory /usr/local/bin > /dev/null 2<&1
-#    git config --global --add safe.directory /var/www/dashboard > /dev/null 2<&1
-#fi
+# set to make repos updated
+if ! grep -q 'safe' /root/.gitconfig > /dev/null 2<&1 ; then
+    git config --global --add safe.directory /usr/local/sbin > /dev/null 2<&1
+    git config --global --add safe.directory /usr/local/bin > /dev/null 2<&1
+    git config --global --add safe.directory /var/www/dashboard > /dev/null 2<&1
+fi
 
 # check if 4.1.6 or greater?
 
@@ -28,22 +28,29 @@ echo === /usr/local/bin: =======================================================
 cd /usr/local/sbin
 #sudo git remote show origin https://github.com/${pgit}/Pi-Star_Binaries_sbin.git
 sudo git remote set-url origin https://github.com/${pgit}/Pi-Star_Binaries_sbin.git
-sudo git pull origin master
+#sudo git pull origin master
+sudo git fetch origin
+sudo git checkout master
 sudo git reset --hard origin/master
 
 echo === /usr/local/bin: ==================================================================
 cd /usr/local/bin
 #sudo git remote show origin https://github.com/${pgit}/Pi-Star_v4_Binaries_Bin.git
 sudo git remote set-url origin https://github.com/${pgit}/Pi-Star_v4_Binaries_Bin.git
-sudo git pull origin master
+#sudo git pull origin master
+sudo git fetch origin
+sudo git checkout master
 sudo git reset --hard origin/master
 
 echo === /var/www/dashboard: ==============================================================
 cd /var/www/dashboard
 #sudo git remote show origin https://github.com/${pgit}/Pi-Star_DV_Dash.git
 sudo git remote set-url origin https://github.com/${pgit}/Pi-Star_DV_Dash.git
-sudo git pull origin master
 sudo git stash save "save user config files"
+#sudo git pull origin master
+sudo git fetch origin
+sudo git checkout master
+#sudo git stash save "save user config files"
 sudo git reset --hard origin/master
 sudo git stash pop
 
